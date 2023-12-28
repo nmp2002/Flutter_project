729e7aec83a3df0328c11a0d2a21b661a48416ce
duongmy2002 <20010970@st.phenikaa-uni.edu.vn>
2023-12-28 11:30:57 +0700

    implement app bar

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -5,9 +5,60 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(child: Text("Homepage")),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFF7F7F7),
+          flexibleSpace: FlexibleSpaceBar(
+            background: Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.location_history,
+                    color: Colors.grey[600],
+                  ),
+                  Container(
+                    width: 4,
+                  ),
+                  Text(
+                    "Phnom Penh",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey[800],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            Stack(children: [
+              Container(
+                height: 36,
+                width: 36,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1.5, color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.white),
+                child: Icon(
+                  Icons.notifications_none_outlined,
+                  color: Colors.grey[600],
+                ),
+              ),
+              const Positioned(
+                  top: 6,
+                  right: 6,
+                  child: Icon(Icons.brightness_1,
+                      size: 10.0, color: Colors.redAccent)),
+            ]),
+            Container(
+              width: 12,
+            )
+          ],
+        ),
       ),
     );
   }
